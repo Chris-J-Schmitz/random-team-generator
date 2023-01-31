141baa1c46aaa5a2d02ebe489c29ec73af5285f5
Chris Schmitz <48893974+Chris-J-Schmitz@users.noreply.github.com>
2023-01-31 07:13:35 -0800

    updated js
    Started js to generate random teams

---
+++ script.js
@@ -0,0 +1,74 @@
+// Variable Definitions
+//Button 
+const generate = document.getElementById("generate-button");
+// Title Definition
+const title = document.getElementById("generate")
+//Lane div definitions
+const offlane = document.getElementById("offlane");
+const jungle = document.getElementById("jungle");
+const midLane = document.getElementById("mid-lane");
+const carry = document.getElementById("carry");
+const support = document.getElementById("support");
+
+// Image definitions
+const offlaneImage = document.getElementById("offlane-image");
+const jungleImage = document.getElementById("jungle-image");
+const midLaneImage = document.getElementById("mid-image");
+const carryImage = document.getElementById("carry-image");
+const supportImage = document.getElementById("support-image");
+
+
+// Images
+const images = ["/assets/Countess.webp","/assets/Crunch.webp","/assets/Dekker.webp","/assets/Drongo.webp","/assets/Countess.webp",
+"/assets/Feng_Mao.webp","/assets/Gadget.webp","/assets/Gideon.webp","/assets/Howitzer.webp","/assets/Kallari.webp",
+"/assets/Khaimera.webp","/assets/Lt_Belica.webp","/assets/Murdock.webp","/assets/Muriel.webp","/assets/Narbash.webp",
+"/assets/Rampage.webp","/assets/Sevarog.webp","/assets/Sparrow.webp","/assets/Steel.webp","/assets/The_Fey.webp",];
+
+const imageIndexLength = 5;
+let imageIndex = [];
+
+// On button click 
+generate.addEventListener("click", function () {
+    // Display names of the roles
+    displayNames();
+    // Generate the random characters 
+    randomIndex()
+    // Display the characters 
+    displayImages();
+});
+
+
+
+//Display the photos of the characters
+function displayImages () {
+    //display images
+    offlaneImage.src = images[imageIndex[0]];
+    jungleImage.src = images[imageIndex[1]];
+    midLaneImage.src = images[imageIndex[2]];
+    carryImage.src = images[imageIndex[3]];
+    supportImage.src = images[imageIndex[4]];
+}
+
+
+// Dispalay the rule names as well as changing the title
+function displayNames () {
+    title.innerHTML = "Good Luck!"
+    generate.innerHTML = "Generate Again"
+    offlane.innerHTML = "Offlane";
+    jungle.innerHTML = "Jungle";
+    midLane.innerHTML = "Mid Lane";
+    carry.innerHTML = "Carry";
+    support.innerHTML = "Support";
+}
+
+
+//Generate a list of 5 numbers to store in the imageIndex array 
+//to represent the 5 random characters
+function randomIndex() {
+
+    for (let i = 0; i < imageIndexLength; i++) {
+        let temp = Math.floor(Math.random() * 20);
+        imageIndex.push(temp);
+    } 
+
+}
